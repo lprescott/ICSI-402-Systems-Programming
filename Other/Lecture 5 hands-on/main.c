d7d8cd5921612f84f0407389f05046b4f53ec31c
Luke Prescott <lukep@nycap.rr.com>
2017-09-12 15:51:26 -0400

    First attempt in class.

--- Other/Lecture 5 hands-on/main.c
+++ Other/Lecture 5 hands-on/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void main( int argc, char *argv[] )  {
 
    if( argc == 2 ) {
       printf("The input file supplied is \"%s\"\n", argv[1]);
 
+      long pos;
       FILE * fp;
       fp = fopen(argv[1], "r");
 
@@ -13,12 +15,24 @@ void main( int argc, char *argv[] )  {
       else printf("\nThe file has been opened successfully.\n");
 
       // Print the first character.
+      fseek(fp, 0, SEEK_SET);
+      pos = ftell(fp);
+      printf("The character in the %ld position is %c.", pos, getc(fp));
 
       //Print the character with offset = 5 from the beginning (also print the offset value)
+      fseek(fp, 5, SEEK_SET);
+      pos = ftell(fp);
+      printf("\nThe character in position %ld is %c.", pos, getc(fp));
 
       //Print the character whose offset is 3 plus the current offset (also print the offset value)
+      fseek(fp, 3, SEEK_CUR);
+      pos = ftell(fp);
+      printf("\nThe character in position %ld is %c.", pos, getc(fp));
 
       //Print the character whose offset is 4 less thatn the current offset (also print the offset value)
+      fseek(fp, -4, SEEK_CUR);
+      pos = ftell(fp);
+      printf("\nThe character in position %ld is %c.", pos, getc(fp));
 
       //Close the input file
       fclose(fp);

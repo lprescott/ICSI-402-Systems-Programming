e194f1650e5bafe37444d5505afeef39f40c587f
invalidargs56 <31902400+invalidargs56@users.noreply.github.com>
2017-10-03 18:43:15 -0400

    Update funcOne.c

--- Assignment 1/funcOne.c
+++ Assignment 1/funcOne.c
@@ -8,48 +8,60 @@
 
 //Parses a log file line into a logline struct
 logline* parseLine(char[] line) {
+   //duplicates the argument line into str
    char str[] = strdup(line);
    
+   //tokenizes str and splits the string by ","
    char *levelP = strtok(str, ",");
    char *timestampP = strtok(NULL, ",");
    char *messageP = strtok(NULL, " ");
    
+   //stores all temp values into a logline struct
    logline parsed = { .level = levelP, .timestamp = timestampP, .message = messageP }
    
+   //returns logline
    return parsed;
 }
 
 //frees up all memory for the list
 void deleteList(loglist* l) {
    
+   //initializes necessary variables
    loglist * head = l;
    loglist * next;
    
+   
+   //loops through list and frees up all memory
    while (current != NULL) {
       next = current->next;
       free(current);
       current = next;
    }
    
+   //sets the initial list to null, freeing all the memory
    l = NULL;
 }
 
 //prints all loglines in a loglist
 void printLines(logList* l) {  
    
+   //initalizes all the temp variables
    logList * next;
    char tempLevel[];
    char tempTimestamp[];
    char tempMessage[];
    
+   //sets all things to their temp things
    tempLevel = l->level;
    tempTimestamp = l->timestamp;
    tempMessage = l->message;
    
+   //print the head data
    printf("%s,%s,%s", tempLevel, tempMessage, tempTimestamp);
  
    nextL = l->next;
    
+   //repeat but for the rest of the list
    while (next != NULL) {
       tempLevel = nextL->level;
       tempTimestamp = nextL->timestamp;

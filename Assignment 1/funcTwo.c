b86f1a422d4b867a4bc03b3c560c155a98258db4
invalidargs56 <31902400+invalidargs56@users.noreply.github.com>
2017-10-03 16:57:12 -0400

    Update funcTwo.c

--- Assignment 1/funcTwo.c
+++ Assignment 1/funcTwo.c
@@ -1,2 +1,9 @@
+//Project: Prog1
+//Names:   Luke Prescott, Rob Rose, Tommy Li
+//Roles:   Leader, Monitor, Recorder Respectively
+//Date:    10/3/2017
+//Course:  ICSI 402
+//Desc:    This file contains the functions mergeLists and sortList to be used in main.c 
+
 loglist* mergeLists(loglist* resultlist, loglist* inlist);
 loglist* sortList(loglist* inlist);

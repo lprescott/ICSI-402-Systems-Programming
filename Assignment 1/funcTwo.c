bfc708d6fcd9a2c9f1c5405752237d07c759725e
tommyli311 <tli3@albany.edu>
2017-10-04 21:44:37 -0400

    Update funcTwo.c
    add comments on potential solution for mergelist function

--- Assignment 1/funcTwo.c
+++ Assignment 1/funcTwo.c
@@ -5,5 +5,20 @@
 //Course:  ICSI 402
 //Desc:    This file contains the functions mergeLists and sortList to be used in main.c 
 
+/*merges the sorted link list "inlist" into "resultlist" 
+and it returns a pointer to the head of the combined list
+*/
 loglist* mergeLists(loglist* resultlist, loglist* inlist);
+
+/* use either a concatenation function where the two log files are listed
+into one major file, or create named create a new file named resultlist, then
+open contents of each files, and copy the contents into each file into result list.
+*?
+
+//http://www.geeksforgeeks.org/c-program-merge-contents-two-files-third-file/
+
+
+/*sorts the provided linked list inlist and 
+returns a pointer to the head of sorted list 
+*/
 loglist* sortList(loglist* inlist);

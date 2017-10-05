26fcc67859e3458cd99b1ebe502089f6d9929f58
invalidargs56 <31902400+invalidargs56@users.noreply.github.com>
2017-10-05 17:43:52 -0400

    Update structs.h

--- Assignment 1/structs.h
+++ Assignment 1/structs.h
@@ -1,1 +1,10 @@
+typedef struct logline{
+  char level[20];
+  char timestamp[20];
+  char message[100];
+} logline_t;
 
+typedef struct loglist {
+  logline_t line;
+  struct loglist *next;
+} loglist_t;

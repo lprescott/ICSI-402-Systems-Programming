a6dff8bf8cb7d22bf433529c3179ddc88b76068b
lprescott <lukep@nycap.rr.com>
2017-10-06 19:58:56 -0400

    Reorganization of project one.

---
+++ Assignment 1 - Attempt 2/structs.h
@@ -0,0 +1,15 @@
+#ifndef STRUCTS_H
+#define STRUCTS_H
+
+typedef struct logline{
+	char level[20];
+	char timestamp[20];
+	char message[100];
+} logline_t;
+
+typedef struct loglist {
+	logline_t line;
+	struct loglist *next;
+} loglist_t;
+
+#endif

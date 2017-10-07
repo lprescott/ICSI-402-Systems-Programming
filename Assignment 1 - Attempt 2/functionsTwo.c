e966ffeab523338307579159421e884a790171db
lprescott <lukep@nycap.rr.com>
2017-10-06 20:09:11 -0400

    Started organizing functions.

---
+++ Assignment 1 - Attempt 2/functionsTwo.c
@@ -0,0 +1,16 @@
+/*
+	Function loglist* mergeLists(loglist* resultlist, loglist* inlist) to merge
+	sorted linked list inlist into resultlist and return a pointer to the head of the
+	combined list.
+*/
+loglist* mergeLists(loglist* resultlist, loglist* inlist){
+	
+}
+
+/*
+	Function loglist* sortList(loglist* inlist) to sort the provided linked list inlist
+	and return a pointer to the head of the sorted list.
+*/
+loglist* sortList(loglist* inlist){
+	
+}

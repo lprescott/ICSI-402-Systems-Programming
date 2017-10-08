7c0a8a0477a903cee7ebdbd91a475ad0cb37ee86
invalidargs56 <31902400+invalidargs56@users.noreply.github.com>
2017-10-08 14:03:39 -0400

    Update functionsTwo.c

--- Assignment 1/functionsTwo.c
+++ Assignment 1/functionsTwo.c
@@ -14,6 +14,38 @@ loglist_t * mergeLists(loglist_t * resultlist, loglist_t * inlist){
 	Function loglist* sortList(loglist* inlist) to sort the provided linked list inlist
 	and return a pointer to the head of the sorted list.
 */
-loglist_t * sortList(loglist_t * inlist){
-	
+void sortedInsert(loglist_t ** inlist, loglist_t * newnode) {
+  loglist_t * current;
+
+  if (*inlist == NULL || (strcmp (*inlist->line.timestamp , newnode->line.timestamp) >= 0)) {
+    newnode->next == inlist;
+    *inlist = newnode;
+  } else {
+    current = *inlist;
+    while (current->next != NULL && (strcmp (current->next->line.timestamp, newnode->line.timestamp) < 0)) {
+      current = current->next;
+    }
+    newnode->next = current->next;
+    current->next = newnode;
+  }
+}
+
+loglist_t * sortList(loglist_t ** inlist) {
+
+	loglist_t * sorted = NULL;
+
+	loglist_t * current = *inlist;
+
+	while (current != NULL) {
+
+		loglist_t *next = current->next;
+
+		sortedInsert(&sorted, current);
+
+		current = current->next;
+
+	}
+
+	return sorted;
+
 }

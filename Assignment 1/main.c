cfe8c467885cf50ead706bb632610e777d2235c8
invalidargs56 <31902400+invalidargs56@users.noreply.github.com>
2017-10-10 18:14:44 -0400

    Update main.c

--- Assignment 1/main.c
+++ Assignment 1/main.c
@@ -1,3 +1,11 @@
+//Project: Prog1
+//Names:   Luke Prescott, Rob Rose, Tommy Li
+//Roles:   Leader, Monitor, Recorder Respectively
+//Date:    10/3/2017
+//Course:  ICSI 402
+//Desc:    This program takes directory full of log files then concatonates them into
+//         a single log file, where the order is based on the time of each log.
+
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>

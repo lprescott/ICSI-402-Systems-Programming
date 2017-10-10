a286fefc3b7f6cb06fcebf3cd189648776f4915c
invalidargs56 <31902400+invalidargs56@users.noreply.github.com>
2017-10-10 18:13:36 -0400

    Update functionsOne.c

--- Assignment 1/functionsOne.c
+++ Assignment 1/functionsOne.c
@@ -10,23 +10,28 @@
 	based on the input string. Note that this function allocates memory.
 */
 logline_t * parseLine(char line []){
+	//Creates a duplicate string before tokenizing
 	char str[128];
 	strcpy(str, line);
-
+	
+	//tokenizes lines to name, time, and message
 	char * levelP = strtok(str, ",");
 	char * timestampP = strtok(NULL, ",");
 	char * messageP = strtok(NULL, ",");
-
+	
+	//checks to make sure that line is free to be used
 	logline_t * parsed = NULL;
 	if((parsed = malloc(sizeof(logline_t))) == NULL){
 		fprintf(stderr, "Unable to allocate memory for new parsed structure.\n");
 		exit(-1);
 	}
-  
-  if (strstr(messageP, "\n") == NULL) {
-    strcat(messageP, "\n");
-  }
-  
+  	
+	//if there is no end of line character, then it puts one on the end of the string
+  	if (strstr(messageP, "\n") == NULL) {
+   	 	strcat(messageP, "\n");
+  	}
+  	
+	//copys the tokens onto the line struct
 	strcpy(parsed->level , levelP);
 	strcpy(parsed->timestamp , timestampP);
 	strcpy(parsed->message , messageP);
@@ -38,13 +43,16 @@ logline_t * parseLine(char line []){
 	Function void deleteList(loglist* l) to free all memory allocated for the list.
 */
 void deleteList(loglist_t * head){
+	//if head is equal to null, then return
 	if (head == NULL){
 		return;
 	}
-
+	
+	//sets current equal to head
 	loglist_t * current = head;
 	loglist_t * next;
-
+	
+	//loops through the list, freeing up data for every variable until current hits null
 	while (current != NULL) {
 		next = current->next;
 		free(current);
@@ -57,7 +65,8 @@ void deleteList(loglist_t * head){
 	Function void printLines(LineList* l) to print all the lines contained in the list.
 */
 void printLines( loglist_t * head){
-
+	
+	//if head is equal to null, then it breaks
 	if (head == NULL){
 		printf("Empty List\n");
 		return;

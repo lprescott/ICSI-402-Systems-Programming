211144055c343125aa11379f5aca807b9c5e038c
tommyli311 <tli3@albany.edu>
2017-10-19 17:01:26 -0400

    comments, gave a description of what each c source file does

---
+++ Assignment 2/archiving.c
@@ -0,0 +1,21 @@
+//Project: 	Prog1
+//Names:   	Luke Prescott, Rob Rose, Tommy Li (lprescott@albany.edu, rwrose@albany.edu, tli3@albany.edu)
+//Roles:   	Leader, Monitor, Recorder Respectively
+//Date:    	10/19/2017
+//Course:  	ICSI 402
+//Desc: contains the two functions, archive which createsan archive file from a set of individual files
+//         unarchive function unpacks an archive file and create
+        
+//Input:   	
+//Output:  	
+//Assumption:	
+
+//archive function
+//void archive(char** filenames, int numFiles, char* archivename)
+
+void archive(char**, int, char*)
+
+//unarchive function
+//unarchive(char* archivefile)
+
+void unarchive(char*)
---
+++ Assignment 2/archivingTwo.c
@@ -0,0 +1,20 @@
+//Project: 	Prog1
+//Names:   	Luke Prescott, Rob Rose, Tommy Li (lprescott@albany.edu, rwrose@albany.edu, tli3@albany.edu)
+//Roles:   	Leader, Monitor, Recorder Respectively
+//Date:    	10/19/2017
+//Course:  	ICSI 402
+//Desc:    	A function to handle the -l flag and a function to handle the - v flag.
+//Input:   	
+//Output:  	
+//Assumption:	
+
+// function to handle -l flag
+
+
+
+
+
+
+
+//function to handle -v flag
+

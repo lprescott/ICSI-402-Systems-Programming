2b75f824a967f94223f50a2ecfdd71db4ab24d66
Luke Prescott <lukep@nycap.rr.com>
2017-10-19 18:10:31 -0400

    Started control structures for main.

--- Assignment 2/main.c
+++ Assignment 2/main.c
@@ -31,16 +31,47 @@
 
 
 int main( int argc, char *argv[] )  {
+    //Variables here:
+    int numOfArgs = 0; //This is number of arguments.
+    numOfArgs = argc - 1; //Calc. that
 
-   if( argc == 2 ) {
-      printf("The argument supplied is %s\n", argv[1]);
-   }
-   else if( argc > 2 ) {
-      printf("Too many arguments supplied.\n");
-   }
-   else {
-      printf("One argument expected.\n");
-   }
+    char * archiveName; 
+
+    // Print for peace of mind.
+    printf("The number of args is: %d\n", numOfArgs);
+
+    //Check for at least two arguments.
+    if (numOfArgs < 2){
+        fprintf(stderr, "There were not enought arguments. ");
+        exit(-1);
+    }
+    else if (strcmp(argv[1], "-a") == 0){
+        //Create an archive.
+        strcpy(archiveName, argv[2]);
+        int numOfFiles = numOfArgs - 2;
+        printf("You supplied %d file names.", numOfFiles);
+
+    }
+    else if (strcmp(argv[1], "-u") == 0){
+        //Unpack an archive.
+        strcpy(archiveName, argv[2]);
+
+    }
+    else if (strcmp(argv[1], "-l") == 0){
+        //Prints an archive size.
+        strcpy(archiveName, argv[2]);
+
+    }
+    else if (strcmp(argv[1], "-v") == 0){
+        //Checks an archive.
+        strcpy(archiveName, argv[2]);
+        int numOfFiles = numOfArgs - 2;
+        printf("You supplied %d file names.", numOfFiles);
+    }
+    else{
+        fprintf(stderr, "Unknown first argument supplied. ");
+        exit(-1);
+    }
 
    return 1;
 }

940730d35ceb4b54b8b9c67856b8452e26fe7ef2
invalidargs56 <rwrose56>
2017-10-20 15:12:03 -0400

    Commented function

--- Assignment 2/archivingSize.c
+++ Assignment 2/archivingSize.c
@@ -4,9 +4,9 @@
 //Date:    	10/19/2017
 //Course:  	ICSI 402
 //Desc:    	this function computes the size of the a file
-//Input:   	
-//Output:  	
-//Assumption:	
+//Input:
+//Output:
+//Assumption:
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,9 +15,11 @@
 #include "archivingSize.h"
 
 /*
-
+  This function finds file size by seeking all the way to the end on the file, then telling the long
+  position the position of the end character. This position is the size of the file in bytes.
 */
 long fileSize(FILE * file){
+    //Position: the variable to be returned, and the position of the end character.S
     long position;
 
     if (file == NULL){

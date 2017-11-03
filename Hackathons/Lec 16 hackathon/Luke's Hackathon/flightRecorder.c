647ff9c3830b621014da2e40ee5a44276fddf125
lprescott <lukep@nycap.rr.com>
2017-11-03 12:36:09 -0400

    Added the rest of the else statements.

--- Hackathons/Lec 16 hackathon/Luke's Hackathon/flightRecorder.c
+++ Hackathons/Lec 16 hackathon/Luke's Hackathon/flightRecorder.c
@@ -146,20 +146,32 @@ int main( int argc, const char* argv[] )
             fclose(flightData);
             printf("\nYour input: ");
         }
+        else if(input == 3){
+            //(d) Count number of airlines for a given airport
+
+        }
+        else if(input == 4){
+            //(e) Print the number of inbound flights for airport x
+
+        }
+        else if(input == 5){
+            //(f) Print the number of inbound flights for each airport
+
+        }
+        else if(input == 6){
+            //(g) Print a sorted list of origin airports based on the number of outbound flights
+
+        }
+        else if(input == 7){
+            //(h) Print a list of origin airports that have at least 2 flights that have a departure time earlier than noon
+            
+        }
         else{
             //End
             printf("\nThe program will end.");
             flag = 0;
         }
     }
-
-    //(c) Read and print info about all flight records in the file
-    //(d) Count number of airlines for a given airport
-    //(e) Print the number of inbound flights for airport x
-    //(f) Print the number of inbound flights for each airport
-    //(g) Print a sorted list of origin airports based on the number of outbound flights
-    //(h) Print a list of origin airports that have at least 2 flights that have a departure time earlier than noon
-
 }
 
 /*

8abcd168784f357c47e50029bbaf7012435a93f1
Luke Prescott <lprescott@users.noreply.github.com>
2017-11-06 16:27:56 -0500

    Update index.c

--- Assignment 3/index.c
+++ Assignment 3/index.c
@@ -1,4 +1,6 @@
+#include <stdio.h>
+
 //A second source file must contain only the function that creates the index.
-void indexer(char ** fullPaths, char * outputFileName){
+void indexer(char ** fullPaths, FILE * ouputFile){
     return;
 }

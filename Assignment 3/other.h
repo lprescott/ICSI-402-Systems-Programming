bf8f0302083729926c16614ca2a7be389f0908b8
Luke Prescott <lprescott@users.noreply.github.com>
2017-11-09 16:43:59 -0500

    Added headers for functions of other.c in other.h

--- Assignment 3/other.h
+++ Assignment 3/other.h
@@ -6,21 +6,20 @@ int isFile(const char * path);
 int isDir(const char * path);
 long fileSize(FILE * file);
 
+void printAll(struct termList ** head);
+void printSorted(termList * inputList, char * outputFileName);
+void printAllCount(fileCountList ** head);
+
 int isTermContained(char * termSupplied, termList * head);
+
 void deleteTermList(termList * head);
+
 void insertFileAndCount(fileCountList ** head, fileCountList * inputList);
 void insertTerm(termList ** head, char * term, fileCountList * filesAndCounts);
+
 termList * readFromFile(char * inputFilePath);
 termList * readFromIndex(char * outputFileName);
+
 termList * sortTerms(termList * inputList);
+
 void mergeSorted(termList ** inputList, termList ** outputList);
-void printSorted(termList * inputList, char * outputFileName);
-
-
-//char * addNumber(char * archiveName, int currentArchive);
-
-/*
-void insertAtFirst(char * term, termList * head);
-void addTerm(termList * head, char * term);
-void printAll(struct termList * head);
-*/

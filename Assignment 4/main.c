b9be46b7697e93d2f1c150b0adf4affa450dc844
Luke Prescott <lukep@nycap.rr.com>
2017-11-14 13:55:57 -0500

    Added another error check
    Also added coments for the function fileExistsAndReadable

--- Assignment 4/main.c
+++ Assignment 4/main.c
@@ -21,12 +21,19 @@ int main( int argc, char *argv[] )  {
 		exit(-1);
 	}
 	else{
+		//Print out supplied arguments
 		printf("\nfirstpass has been called.\n");
 		printf("The supplied instructionset filename: \"%s\".\n", argv[1]);
 		printf("The supplied programfile filename: \"%s\".\n", argv[2]);
 		printf("The supplied hashtablesize: \"%s\".\n", argv[3]);
 	}
 	
+	//Check if the supplied hashtablesize is not a number
+	if(atoi(argv[3]) == 0){
+		fprintf(stderr, "\nThe supplied hashtablesize is not a number.\nExiting...\n");
+		exit(-1);
+	}
+
 	//Assign int to converted command line arg.
 	hashTableSize = atoi(argv[3]);
 
--- Assignment 4/other.c
+++ Assignment 4/other.c
@@ -1,7 +1,12 @@
 //included libraries
 #include <unistd.h>
 
+/*
+	This function returns a 1 if the supplied file name exists and is readable by using the access function. 
+	If the supplied file name does not exist or is not readable, the function returns 0.
+*/
 int fileExistsAndReadable(char * filename){
+	//F_OK|R_OK are used for checking if the file is existing (F_OK) and readable (R_OK)
 	if(access(filename, F_OK|R_OK) != -1) return 1;
 	else return 0;
 }

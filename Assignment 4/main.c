434f9547fdacdedbf174632e9e52aaf753e9434b
Luke Prescott <lprescott@users.noreply.github.com>
2017-11-14 17:09:57 -0500

    Looping through instructionSetFile and
    adding to tempNode

--- Assignment 4/main.c
+++ Assignment 4/main.c
@@ -3,17 +3,30 @@
 //included libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //included headers
 #include "other.h"
 #include "BSTheaders.h"
 #include "hashHeaders.h"
 
+//Constants
+#define LINESIZE 100
+
 //main method
 int main( int argc, char *argv[] )  {
 	//variables
 	int hashTableSize = 0; //an integer to hold the hash table size supplied
-	
+	FILE * instructionSetFile; //A FILE type for the instructionSet input file
+	FILE * programFile; //A FILE type for the program input file
+	char line [LINESIZE]; //A char pointer that will store each line in the instructionSet input file
+	char tempInstruction [6]; //The temp five character long instruction
+	unsigned char tempOpcode; //The temp unsigned char to hold the opcode integer 
+	unsigned char tempFormat; //The temp unsigned char to hold the format integer
+	char * token; //A char pointer used when tokenizing a line in the instructionSetFile
+	instructionSet * head = NULL; //A instructionSet pointer to the head node of the used BST
+	instructionSet * tempNode; //A instructionSet pointer to the head node of the temp BST node
+
 	//Check number of arguments
 	if(argc < 4){
 		fprintf(stderr, "There were only %d arguments supplied. Expected 4.\nExiting...\n", argc);
@@ -59,10 +72,51 @@ int main( int argc, char *argv[] )  {
 	}
 
 	//Open and check the instructionset filename
+	instructionSetFile = fopen(argv[1], "r");
+	if(instructionSetFile == NULL){
+        	fprintf(stderr, "Could not allocate memory for instructionSetFile.\nExiting...\n");
+        	exit(1);
+	}
 
+	printf("\n");
 	//Parse line by line, inserting into BST
+ 	while (fgets(line, LINESIZE, instructionSetFile)) {
+		tempNode = NULL; //Set the tempNode to NULL at every loop
+
+		//Parse (and print) the lines of the instructionSetFile
+		//printf("%s", line);
+		token = strdup(line);
+
+		//The instruction is the line up to the first space
+		token = strtok(token, " ");
+		strcpy(tempInstruction, token);
+		//printf("%s ", tempInstruction);
+
+		//The opcode is the rest of the line up to the second space
+		token = strtok(NULL, " ");
+		tempOpcode = atoi(token);
+		//printf("%d ", tempOpcode);
+
+		//The format is the rest of the line up to the newline char
+		token = strtok(NULL, "\n");
+		tempFormat = atoi(token);
+		//printf("%d\n", tempFormat);
+
+		//create tempNode
+		tempNode = newInstructionSet(tempInstruction, tempOpcode, tempFormat);
+
+		//print attributes of tempNode
+		printf("%s ", tempNode->instruction);
+		printf("%d ", tempNode->opcode);
+		printf("%d\n", tempNode->format);
+
+		
+		
+	}
 
 	//Print the BST (in-order)
 
+	//Close opened files and print done
+	fclose(instructionSetFile);
 	printf("\nDone.\n");
 }

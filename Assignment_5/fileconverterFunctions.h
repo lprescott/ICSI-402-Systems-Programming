b7d446ce7001eed6135085e3b243753f8163bf8f
tommyli311 <tli3@albany.edu>
2017-12-03 18:24:53 -0500

    deleted the struct containing student information

--- Assignment_5/fileconverterFunctions.h
+++ Assignment_5/fileconverterFunctions.h
@@ -1,12 +1,3 @@
-typedef struct studentRecord {
-	unsigned char l1;
-	unsigned char l2;
-	char * firstName;
-	char * lastName;
-	int id;
-	int GPA;
-} studentRecord;
-
 int isBinaryFile(char * testFile);
 int isReadableFile(char * testFile);
 

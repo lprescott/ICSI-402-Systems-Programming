3e1bffef0b458f56f79d8b7368db6e0c46d32635
invalidargs56 <rwrose56@gmail.com>
2017-12-03 17:45:00 -0500

    created studentRecord struct

--- Assignment_5/fileconverterFunctions.h
+++ Assignment_5/fileconverterFunctions.h
@@ -1,3 +1,12 @@
+typedef struct studentRecord {
+	unsigned char l1;
+	unsigned char l2;
+	char * firstName;
+	char * lastName;
+	int id;
+	int GPA;
+} studentRecord;
+
 int isBinaryFile(char * testFile);
 int isReadableFile(char * testFile);
 

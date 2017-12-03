d17697ed74e300b013a9485332cb30ef83d35ebc
invalidargs56 <rwrose56@gmail.com>
2017-12-03 14:43:54 -0500

    organized fileconverter and created fileconverterFunctions.c

--- Assignment_5/fileconverter.c
+++ Assignment_5/fileconverter.c
@@ -2,19 +2,82 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+#include "fileconverterFunctions.h"
+
 
 int main( int argc, char *argv[] )  {
+	
+	printf("%d\n", argc);
 
-   if( argc == 2 ) {
-      printf("The argument supplied is %s\n", argv[1]);
-   }
-   else if( argc > 2 ) {
-      printf("Too many arguments supplied.\n");
-   }
-   else {
-      printf("One argument expected.\n");
-    }
+	//NON INPUT REDIRECTION CASES
+	//Case 1: flag infile outfile
+	if ( argc == 4 ) {
+		//Case 1 a) flag is -t
+		if ( strcmp(argv[1], "-t") == 0 ) {
+			
+			//Check if the second argument is a readable file
+			/*if ( !isReadableFile( argv[2] ) ) {
+				fprintf(stderr, "ERROR: first file given isn't readable");
+				exit(-1);
+			}*/
+			
+			//Check if the third argument is a binary file
+			/*if ( !isBinaryFile ( argv[3] ) ) {
+				fprintf(stderr, "ERROR: second file given isn't binary");
+				exit(-1);
+			}*/
+			
+			//Call file converter for binary
+			//convertToBinary( argv[2], argb[3] );
+			
+		}
+		//Case 1 b) flag is -b
+		if ( strcmp(argv[1], "-b") == 0 ) {
+			
+			//Check if the second argument is a binary file
+			/*if ( !isBinaryFile( argv[2] ) ) {
+				fprintf(stderr, "ERROR: first file given isn't binary");
+				exit(-1);
+			}*/
+			
+			//Check if the third argument is a readable file
+			/*if ( !isReadableFile ( argv[3] ) ) {
+				fprintf(stderr, "ERROR: second file given isn't readable");
+				exit(-1);
+			}*/
+			
+			//Call file converter for binary
+			//convertToReadable( argv[2], argb[3] );
+			
+		}
+		
+	} 
+	//Case 2: flag is -s
+	else if ( argc == 3 ) {
+		
+		if ( strcmp(argv[1], "-s") == 0 ) {
+			
+			//Check if the second argument is a binary file
+			/*if ( !isBinaryFile( argv[2] ) ) {
+				fprintf(stderr, "ERROR: first file given isn't binary");
+				exit(-1);
+			}*/
+			
+			//call printDetails()
+			//printDetails( argv[2] );
+			
+		}
+		
+	}
+	
+	/*INPUT REDIRECTION CASES
+	
+	else if ( argc == 5 ) {
+		
+	}
     
+	*/
+	
     //Exit with 0, program was succesfully completed
 	exit(0);
 }
---
+++ Assignment_5/fileconverterFunctions.c
@@ -0,0 +1,36 @@
+#include "fileconverterFunctions.h"
+
+/*
+
+*/
+int isBinaryFile(char * testFile) {
+	
+}
+
+/*
+
+*/
+int isReadableFile(char * testFile) {
+	
+}
+
+/*
+
+*/
+void convertToReadable(char * inputFileName, char * outputFileName) {
+	
+}
+
+/*
+
+*/
+void convertToBinary(char * inputFileName, char * outputFileName) {
+	
+}
+
+/*
+
+*/
+void printDetails(char * inputFileName) {
+	
+}

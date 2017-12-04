ce26a731ed1a58e440c544c2ed97c2ea6ef59874
Luke Prescott <lukep@nycap.rr.com>
2017-12-03 19:01:07 -0500

    working copy of fileconverterfunctions

--- Assignment_5/fileconverterFunctions.c
+++ Assignment_5/fileconverterFunctions.c
@@ -7,7 +7,56 @@
 
 */
 void convertToReadable(char * inputFileName, char * outputFileName) {
-	
+    FILE * binFILE; //FILE pointer to hold binary file 
+    FILE * txtFILE; //FILE pointer to hold text file 
+    
+    //Temporary variables to hold student record attributes
+    unsigned char templ1;
+	unsigned char templ2;
+	char * tempfirstName = malloc(sizeof(char *));
+	char * templastName = malloc(sizeof(char *));
+	int tempid;
+	int tempGPA;
+    
+    //Check if files are openable
+    binFILE = fopen(inputFileName, "rb");
+    if(binFILE == NULL){
+        fprintf(stderr, "Could not allocate memory for the binFILE.\n");
+        exit(-1);
+    }
+
+    txtFILE = fopen(outputFileName, "w");
+    if(binFILE == NULL){
+        fprintf(stderr, "Could not allocate memory for the txtFILE.\n");
+        exit(-1);
+    }
+
+    //Loop to read through binary file
+    while(1){
+		if (fread(&templ1, sizeof(templ1), 1, binFILE) == 0) return; //read l1
+        tempfirstName = malloc(sizeof(char) * templ1 + 1); //Allocate mem for first name
+        if (fread(tempfirstName, sizeof(char), templ1, binFILE) == 0) return; //read tempfirstName
+        if (fread(&templ2, sizeof(templ2), 1, binFILE) == 0) return; //read l2
+        templastName = malloc(sizeof(char) * templ2 + 1); //Allocate mem for last name
+        if (fread(templastName, sizeof(char), templ2, binFILE) == 0) return; //read templastName
+        if (fread(&tempid, sizeof(tempid), 1, binFILE) == 0) return; //read tempid
+        if (fread(&tempGPA, sizeof(tempGPA), 1, binFILE) == 0) return; //read tempGPA
+
+		tempfirstName[strlen(tempfirstName) ] = '\0';
+		templastName[strlen(templastName) ] = '\0';
+
+		//Print attributes to txt file
+		printf("%d %s %d %s %d %d\n", templ1, tempfirstName, templ2, templastName, tempid, tempGPA);
+        fprintf(txtFILE, "%s %s %d %d\n", tempfirstName, templastName, tempid, tempGPA);
+
+        //Free strings
+        free(tempfirstName);
+        free(templastName);
+    }
+    
+    //Close files
+    fclose(binFILE);
+    fclose(txtFILE);
 }
 
 /*
@@ -32,7 +81,7 @@ void convertToBinary(char * inputFileName, char * outputFileName) {
 	}
 	
 	//checks if outputFile could be opened
-	if ((outputFile = fopen(outputFileName, "w")) == NULL) {
+	if ((outputFile = fopen(outputFileName, "wb")) == NULL) {
 		fprintf(stderr, "ERROR could not open outputFile file.\n");
 		exit(-1);
 	}
@@ -80,6 +129,11 @@ void convertToBinary(char * inputFileName, char * outputFileName) {
 			
 		}
 		
+		/*
+		firstName[strlen(firstName)-1] = '\0';
+		lastName[strlen(lastName)-1] = '\0';
+		*/
+
 		//writes all necessary variables to gpa
 		fwrite(&l1, sizeof(l1), 1, outputFile);
 		fwrite(firstName, sizeof(char), l1, outputFile);
